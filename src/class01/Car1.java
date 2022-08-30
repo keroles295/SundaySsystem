e7e57143a7d4a3e842d34c1c35404cab2576545f
keroles295 <kerolesbotros295@gmail.com>
2022-08-29 23:53:53 -0400

    the first Model has been added

---
+++ src/class01/Car1.java
@@ -0,0 +1,8 @@
+package class01;
+
+public class Car1 {
+    String name;
+    int door;
+    String color;
+    String model;
+}
---
+++ src/class01/Main.java
@@ -0,0 +1,11 @@
+package class01;
+
+public class Main {
+    public static void main(String[] args) {
+        Car1 car1=new Car1();
+        car1.color="Green";
+        car1.door=4;
+        car1.model="X6";
+        car1.name="BMW";
+    }
+}
