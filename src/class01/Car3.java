49d3008a4e905aec3bfa6cc7a132917fc948e210
keroles295 <kerolesbotros295@gmail.com>
2022-08-30 00:43:38 -0400

    Car 3 has been added

---
+++ src/class01/Car3.java
@@ -0,0 +1,9 @@
+package class01;
+
+public class Car3 {
+    String color;
+    int door;
+    String name;
+    String model;
+
+}
--- src/class01/Main.java
+++ src/class01/Main.java
@@ -7,5 +7,15 @@ public class Main {
         car1.door=4;
         car1.model="X6";
         car1.name="BMW";
+
+
+        //added car3
+        Car3 car3=new Car3();
+        car3.color="Gray";
+        car3.door=8;
+        car3.model="Crolla";
+        car3.name="Toyota";
+        
+
     }
 }
