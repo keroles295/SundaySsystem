0d4eb14b60f2a0f1adf4249feb283bdd72777665
keroles295 <kerolesbotros295@gmail.com>
2022-08-30 00:25:00 -0400

    Car 2 has been added

--- src/class01/Main.java
+++ src/class01/Main.java
@@ -7,5 +7,13 @@ public class Main {
         car1.door=4;
         car1.model="X6";
         car1.name="BMW";
+
+
+        //adding car2
+        Car2 car2=new Car2();
+        car2.color="Black";
+        car2.door=6;
+        car2.name="Honda";
+        car2.model="Civic";
     }
 }
